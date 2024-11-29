43d802474fa33e58ba73b313aa6cad9a0cd961d7
Fernando Emed <ferweb@gmail.com>
2024-11-29 11:00:32 -0300

    First commit

--- EKS/backend.tf
+++ EKS/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "mytodoappbucket"
+    bucket = "ferwebbucket"
     key    = "eks/terraform.tfstate"
-    region = "us-east-2"
+    region = "us-east-1"
   }
 }
--- jenkins-server/backend.tf
+++ jenkins-server/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "mytodoappbucket"
+    bucket = "ferwebbucket"
     key    = "jenkins/terraform.tfstate"
-    region = "us-east-2"
+    region = "us-east-1"
   }
 }
