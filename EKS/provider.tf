88f7734bc174eb2de9576608c5053e2d128a6fc7
Fernando Emed <ferweb@gmail.com>
2024-11-29 18:35:41 -0300

    data & provider file changes

--- EKS/provider.tf
+++ EKS/provider.tf
@@ -1,3 +1,14 @@
-provider "aws" {
-  region = "us-east-1"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.20.0"
+      #region = "us-east-1"
+    }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.0.1"
+    }
+  }
 }
