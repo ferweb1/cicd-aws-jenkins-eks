9afebd62a801187eed3e4f01b1658924193be751
Fernando Emed <ferweb@gmail.com>
2024-11-29 18:40:38 -0300

    Update data tf

--- EKS/data.tf
+++ EKS/data.tf
@@ -1,7 +1,9 @@
 data "aws_availability_zones" "azs" {}
 
 data "aws_eks_cluster" "cluster" {
-  name = module.eks.cluster_id
+  #name = module.eks.cluster_id
+  name = module.eks.cluster_name
+  depends_on = [module.eks.cluster_name]
 }
 
 data "aws_eks_cluster_auth" "cluster" {
